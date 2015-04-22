ed2a14678a570b232d4a7d53183dbc8288d41eda
Alexandre Leuck <aleuck@inf.ufrgs.br>
2015-04-22 19:26:48 -0300

    chess board and pieces movimentations OO style

---
+++ chessboard.js
@@ -0,0 +1,235 @@
+(function (exports) {
+    var extend = require('./extend.js').extend;
+    var Team = {
+        WHITE : 1,
+        BLACK : -1
+    };
+
+    function insideBoard(x, y) {
+        return (x >= 0 && x <= 7 && y >= 0 && y <= 7);
+    }
+
+
+    //////////////////////////////////////////////////
+    // Classe Piece
+    //////////////////////////////////////////////////
+
+    // Construtor
+    function Piece() {
+        this.board = null;
+        this.team = null;
+        this.position = null;
+        this.type = null;
+    }
+    // Propriedades default e métodos
+    Piece.prototype = {
+        generate: function () {
+            return [];
+        },
+        isOpponent: function (piece) {
+            return (piece.team === -this.team);
+        }
+    };
+
+
+    //////////////////////////////////////////////////
+    // Classe Pawn
+    //////////////////////////////////////////////////
+
+    var Pawn = extend(
+        // Classe pai
+        Piece,
+        // Construtor
+        function (board, team, position) {
+            this.board = board;
+            this.team = team;
+            this.position = position;
+        },
+        // Propriedades default e métodos
+        {
+            'generate' : function () {
+                var moves = [],
+                    myX = this.position[0],
+                    myY = this.position[1],
+                    d = this.team,
+                    x,
+                    y,
+                    piece;
+
+                // movement to 1 forward
+                x = myX + d;
+                y = myY;
+                if (!this.board.cells[x][y]) {
+                    moves.push([x, y]);
+                }
+
+                // normal capture to right
+                y = myY + 1;
+                if (insideBoard(x, y)) {
+                    piece = this.board.cells[x][y];
+                    if (piece && this.isOpponent(piece)) {
+                        moves.push([x, y]);
+                    }
+                }
+
+                // normal capture to left
+                y = myY - 1;
+                    if (insideBoard(x, y)) {
+                    piece = this.board.cells[x][y];
+                    if (piece && this.isOpponent(piece)) {
+                        moves.push([x, y]);
+                    }
+                }
+
+                return moves;
+            }
+        }
+    );
+
+
+    //////////////////////////////////////////////////
+    // Classe Knight
+    //////////////////////////////////////////////////
+
+    var Knight = extend(
+        // Classe pai
+        Piece,
+        // Construtor
+        function (board, team, position) {
+            this.board = board;
+            this.team = team;
+            this.position = position;
+        },
+        // Propriedades default e métodos
+        {
+            '_gen' : function (moves, x, y) {
+                // if valid position
+                if (insideBoard(x, y)) {
+                    var piece = this.board.cells[x][y];
+                    if (!piece || this.isOpponent(piece)) {
+                        moves.push([x, y]);
+                    }
+                }
+            },
+            'generate' : function () {
+                var moves = [],
+                    myX = this.position[0],
+                    myY = this.position[1];
+                this._gen(moves, myX + 1, myY + 2);
+                this._gen(moves, myX + 1, myY - 2);
+                this._gen(moves, myX - 1, myY + 2);
+                this._gen(moves, myX - 1, myY - 2);
+                this._gen(moves, myX + 2, myY + 1);
+                this._gen(moves, myX + 2, myY - 1);
+                this._gen(moves, myX - 2, myY + 1);
+                this._gen(moves, myX - 2, myY - 1);
+                return moves;
+            }
+        }
+    );
+
+
+    //////////////////////////////////////////////////
+    // Classe Queen
+    //////////////////////////////////////////////////
+
+    var Queen = extend(
+        // Classe pai
+        Piece,
+        // Construtor
+        function (board, team, position) {
+            this.board = board;
+            this.team = team;
+            this.position = position;
+        },
+        // Propriedades default e métodos
+        {
+            '_gen' : function (moves, x, y, xDir, yDir) {
+                var piece;
+                x += xDir;
+                y += yDir;
+                // While inside board
+                while (insideBoard(x, y)) {
+                    // If position has piece
+                    piece = this.board.cells[x][y];
+                    if (piece) {
+                        if (this.isOpponent(piece)) {
+                            moves.push([x, y]);
+                        }
+                        break;
+                    }
+                    // Add free position, go to next position
+                    moves.push([x, y]);
+                    x += xDir;
+                    y += yDir;
+                }
+            },
+            'generate' : function () {
+                var moves = [],
+                    myX = this.position[0],
+                    myY = this.position[1];
+                // Eight directions, counter-clockwise
+                this._gen(moves, myX, myY,  1,  0);
+                this._gen(moves, myX, myY,  1, -1);
+                this._gen(moves, myX, myY,  0, -1);
+                this._gen(moves, myX, myY, -1, -1);
+                this._gen(moves, myX, myY, -1,  0);
+                this._gen(moves, myX, myY, -1,  1);
+                this._gen(moves, myX, myY,  0,  1);
+                this._gen(moves, myX, myY,  1,  1);
+                return moves;
+            }
+        }
+    );
+
+
+    //////////////////////////////////////////////////
+    // Classe Board
+    //////////////////////////////////////////////////
+
+    // Construtor
+    function Board(state) {
+        this.cells = [[], [], [], [], [], [], [], []];
+        this.myPieces = [];
+        var PIECES = {
+            p: Pawn,
+            q: Queen,
+            n: Knight
+        },
+            my_team = state.who_moves,
+            c = state.board,
+            i = 0,
+            x, y, ch, cls, team, piece;
+        for (x = 7; x >= 0; x -= 1) {
+            for (y = 0; y < 8; y += 1) {
+                if (c[i] !== '.') {
+                    ch = c[i].toLowerCase();
+                    cls = PIECES[ch];
+                    team = ch === c[i] ? Team.BLACK : Team.WHITE;
+                    piece = new cls(this, team, [x, y]);
+                    this.cells[x][y] = piece;
+                    if (team === my_team) {
+                        this.myPieces.push(piece);
+                    }
+                }
+                i += 1;
+            }
+        }
+    }
+    // Propriedades default e métodos
+    Board.prototype = {
+        isEmpty: function (pos) {
+            return (this.cells[pos[0]][pos[1]] == undefined);
+        },
+        generate: function () {
+            var moves = [];
+            this.myPieces.forEach(function (piece) {
+                var ms = piece.generate();
+                
+                moves.push.apply(moves, ms = ms.map(function (pos) { return [piece.position, pos]; }));
+            });
+            return moves;
+        }
+    };
+    exports.Board = Board;
+}(exports));
