fc0ba15f71a0b9d009b1e32d2ddd59090c4e8f7b
Aleuck <aleuck2004@gmail.com>
2015-04-24 11:27:45 -0300

    JSLint on RandomBot

--- RandomBot.js
+++ RandomBot.js
@@ -1,4 +1,5 @@
 (function (exports) {
+    'use strict';
     var extend = require('./extend.js').extend,
         Board = require('./chessboard.js').Board,
         LiacBot = require('./base_client.js').LiacBot,
@@ -14,6 +15,7 @@
             {
                 name: "Felipe & Leuck's Random Bot",
                 onMove: function (state) {
+                    console.log(state.board);
                     console.log("Generating a move...");
                     var board = new Board(state),
                         moves,
@@ -24,48 +26,49 @@
                     moves = board.generate();
                     move = moves[Math.floor(Math.random() * moves.length)];
                     this.last_move = move;
-                    this.sendMove(move.from,move.to);
+                    this.sendMove(move.from, move.to);
                 },
                 onGameOver: function (state) {
                     console.log('Game Over');
+                    console.log(state);
+                    console.log('---------');
                 }
             }
         );
     exports.RandomBot = RandomBot;
+
+
+    //////////////////////////////////////////////////
+    // Main (para quando é executado diretamente)
+    //////////////////////////////////////////////////
+
+    function main() {
+        var bot = new RandomBot(),
+            portIdx = process.argv.indexOf("-p"),
+            hostIdx = process.argv.indexOf("-h"),
+            port,
+            host;
+        if (process.argv.indexOf("--help") !== -1) {
+            console.log("Usage: node RandomBot.js [-p PORT] [-h HOST]");
+            process.exit();
+        }
+        if (portIdx > 0) {
+            port = parseInt(process.argv[portIdx + 1], 10);
+        }
+        if (hostIdx > 0) {
+            host = process.argv[hostIdx + 1];
+        }
+        if (port) {
+            bot.port = port;
+        }
+        if (host) {
+            bot.ip = host;
+        }
+        bot.start();
+    }
+
+    if (require.main === module) {
+        main();
+    }
+
 }(exports));
-
-
-
-//////////////////////////////////////////////////
-// Main (para quando é executado diretamente)
-//////////////////////////////////////////////////
-
-function main() {
-    var RandomBot = exports.RandomBot;
-    var bot = new RandomBot();
-    var portIdx = process.argv.indexOf("-p");
-    var hostIdx = process.argv.indexOf("-h");
-    var port;
-    var host;
-    if (process.argv.indexOf("--help") !== -1) {
-        console.log("Usage: node RandomBot.js [-p PORT] [-h HOST]");
-        process.exit();
-    }
-    if (portIdx > 0) {
-        port = parseInt(process.argv[portIdx + 1]);
-    }
-    if (hostIdx > 0) {
-        host = process.argv[hostIdx + 1];
-    }
-    if (port) {
-        bot.port = port;
-    }
-    if (host) {
-        bot.ip = host;
-    }
-    bot.start();
-}
-
-if (require.main === module) {
-    main();
-}
