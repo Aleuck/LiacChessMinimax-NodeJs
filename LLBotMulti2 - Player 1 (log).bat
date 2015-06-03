db223515097d1038a5616196eaf5118700815883
felipeal <limalipe@hotmail.com>
2015-06-03 02:16:35 -0300

    Named log

--- LLBotMulti2 - Player 1 (log).bat
+++ LLBotMulti2 - Player 1 (log).bat
@@ -1,4 +1,9 @@
-echo NOVA PARTIDA >> log.txt
+@echo off
+set /p id=ADVERSARIO: 
+echo -- NOVA PARTIDA -- >> log.txt
 time /t >> log.txt
+echo %id% >> log.txt
+echo PECAS BRANCAS
+@echo on
 node LLBotMultiOrchestrator2.js -p 50100 >> log.txt
 pause
--- LLBotMulti2 - Player 2 (log).bat
+++ LLBotMulti2 - Player 2 (log).bat
@@ -1,4 +1,9 @@
-echo NOVA PARTIDA >> log.txt
+@echo off
+set /p id=ADVERSARIO: 
+echo -- NOVA PARTIDA -- >> log.txt
 time /t >> log.txt
+echo %id% >> log.txt
+echo PECAS PRETAS
+@echo on
 node LLBotMultiOrchestrator2.js -p 50200 >> log.txt
 pause
